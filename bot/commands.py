590445bd9208dd0873c39465029f8b44f2ffb6d6
miladcryptoexchange-creator <miladcryptoexchange@gmail.com>
2026-07-31 09:44:03 +0430

    Update commands.py

---
+++ bot/commands.py
@@ -0,0 +1,24 @@
+"""
+Avesta Platform - Bot Commands
+"""
+
+COMMANDS = {
+    'start': {
+        'description': 'Start the bot and create account',
+        'handler': 'start'
+    },
+    'wallet': {
+        'description': 'Show AVN wallet balance',
+        'handler': 'wallet_command'
+    },
+    'mining': {
+        'description': 'Show mining status',
+        'handler': 'mining_command'
+    },
+    'referral': {
+        'description': 'Show referral link',
+        'handler': 'referral_command'
+    },
+    'help': {
+        'description': 'Show help message',
+        'handler': '
